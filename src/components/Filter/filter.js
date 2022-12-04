9e2fa3a141948be491dff7b1e16968b57ee81cad
Abhay Sharma <abhaysharmajr@gmail.com>
2022-12-04 18:52:39 +0530

    filter part 3

--- src/components/Filter/filter.js
+++ src/components/Filter/filter.js
@@ -113,104 +113,121 @@ export default function Filter() {
     },
   ];
 
-
   const optionStyle = {
     fontWeight: "bold",
     border: "none",
   };
 
-  const [location,setLocation]=useState("All");
-  const [date,setDate]=useState("");
-  const [price,setPrice]=useState("All");
-  const [propType,setPropType]=useState("All")
-  const result={
-    location:location,
-    date:date,
-    price:price,
-    propType:propType,
-  }
+  const [location, setLocation] = useState("All");
+  const [date, setDate] = useState("");
+  const [price, setPrice] = useState("All");
+  const [propType, setPropType] = useState("All");
+  const result = {
+    location: location,
+    date: date,
+    price: price,
+    propType: propType,
+  };
 
   const [item, setItem] = useState(data);
   function filterItem(firstFilter) {
-    if((firstFilter.location!=="All")&&(firstFilter.propType!=="All")){
-      let filterArr1 = data.filter((ele1) =>{
-
-        return (ele1?.city === firstFilter.location)&&(ele1.propertyType===firstFilter.propType);
-       } 
-       );
-       setItem(filterArr1);
-    }else if((firstFilter.propType==="All")&&(firstFilter.location!=="All")){
-      let filterArr1 = data.filter((ele1) =>{
-
-        return (ele1.propertyType===firstFilter.propType);
-       } 
-       );
-       setItem(filterArr1);
-    }else if((firstFilter.propType!=="All")&&(firstFilter.location==="All")){
-      let filterArr1 = data.filter((ele1) =>{
-
-        return (ele1?.city === firstFilter.location);
-       } 
-       );
-       setItem(filterArr1);
-    }else{
+    if (firstFilter.location !== "All" && firstFilter.propType !== "All") {
+      let filterArr1 = data.filter((ele1) => {
+        return (
+          ele1?.city === firstFilter.location &&
+          ele1.propertyType === firstFilter.propType
+        );
+      });
+      setItem(filterArr1);
+    } else if (
+      firstFilter.propType === "All" &&
+      firstFilter.location !== "All"
+    ) {
+      let filterArr1 = data.filter((ele1) => {
+        return ele1?.city === firstFilter.location;
+      });
+      setItem(filterArr1);
+    } else if (
+      firstFilter.propType !== "All" &&
+      firstFilter.location === "All"
+    ) {
+      let filterArr1 = data.filter((ele1) => {
+        return ele1.propertyType === firstFilter.propType;
+      });
+      setItem(filterArr1);
+    } else {
       setItem(data);
     }
-      
-    
-    
-    
   }
   return (
     <div>
       <div className="filter-container">
         <form className="filter-form">
-        <div>
-        <p>Location</p>
-        <select id="property-location" style={optionStyle} onChange={(e)=>setLocation(e.target.value)}>
-          <option value="All">All</option>
-          <option value="NY">New York, USA</option>
-          <option value="FL">Florida, USA</option>
-          <option value="TX">Texas, USA</option>
-          <option value="CA">California, USA</option>
-          <option value="MP">Mississippi, USA</option>
-          <option value="VG">Verginia, USA</option>
-        </select>
-      </div>
-      <div>
-        <p>When</p>
-        <input type="text" placeholder="Select Move-in Date" style={optionStyle}
-        onChange={(e) => setDate(e.target.value)}
-        onFocus={(e) => (e.target.type = "date")}
-        onBlur={(e) => (e.target.type = "text")}
-        />
-      </div>
-      <div>
-        <p>Price</p>
-        <select id="property-rent" style={optionStyle} onChange={(e)=>setPrice(e.target.value)}>
-          <option value="All">All</option>
-          <option value="1">Less then $500</option>
-          <option value="2">$500-$1500</option>
-          <option value="3">$1500-$2500</option>
-          <option value="4">$2500-$4000</option>
-          <option value="5">More then $4000</option>
-        </select>
-      </div>
-      <div>
-        <p>Property Type</p>
-        <select id="property-type" style={optionStyle} onChange={(e)=>setPropType(e.target.value)}>
-          <option value="All">All</option>
-          <option value="Houses">Houses</option>
-          <option value="Flats">Flats</option>
-          <option value="Apartments">Apartments</option>
-        </select>
-      </div>
-      <Button variant="contained" onClick={()=>filterItem(result)} className="search-button">Search</Button>
+          <div>
+            <p>Location</p>
+            <select
+              id="property-location"
+              style={optionStyle}
+              onChange={(e) => setLocation(e.target.value)}
+            >
+              <option value="All">All</option>
+              <option value="NY">New York, USA</option>
+              <option value="FL">Florida, USA</option>
+              <option value="TX">Texas, USA</option>
+              <option value="CA">California, USA</option>
+              <option value="MP">Mississippi, USA</option>
+              <option value="VG">Verginia, USA</option>
+            </select>
+          </div>
+          <div>
+            <p>When</p>
+            <input
+              type="text"
+              placeholder="Select Move-in Date"
+              style={optionStyle}
+              onChange={(e) => setDate(e.target.value)}
+              onFocus={(e) => (e.target.type = "date")}
+              onBlur={(e) => (e.target.type = "text")}
+            />
+          </div>
+          <div>
+            <p>Price</p>
+            <select
+              id="property-rent"
+              style={optionStyle}
+              onChange={(e) => setPrice(e.target.value)}
+            >
+              <option value="All">All</option>
+              <option value="1">Less then $500</option>
+              <option value="2">$500-$1500</option>
+              <option value="3">$1500-$2500</option>
+              <option value="4">$2500-$4000</option>
+              <option value="5">More then $4000</option>
+            </select>
+          </div>
+          <div>
+            <p>Property Type</p>
+            <select
+              id="property-type"
+              style={optionStyle}
+              onChange={(e) => setPropType(e.target.value)}
+            >
+              <option value="All">All</option>
+              <option value="Houses">Houses</option>
+              <option value="Flats">Flats</option>
+              <option value="Apartments">Apartments</option>
+            </select>
+          </div>
+          <Button
+            variant="contained"
+            onClick={() => filterItem(result)}
+            className="search-button"
+          >
+            Search
+          </Button>
         </form>
-      
+      </div>
+      <DisplayProperties result={result} item={item} />
     </div>
-    <DisplayProperties result={result} item={item}/>
-    </div>
-    
   );
 }
